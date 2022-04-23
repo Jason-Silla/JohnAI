ca0bdc0d285dcd8a989d43c78c569fc8f3d2cdf1
Jason Silla <92275622+Jason-Silla@users.noreply.github.com>
2022-04-23 16:40:35 -0400

    Please check CHANGELOG

--- mathx.py
+++ mathx.py
@@ -2,19 +2,19 @@
 ### https://github.com/Jason-Silla/JohnAI ###
 
 class Fraction:
-    numerator = 1
-    denominatior = 1
 
-    def __init__(self, *info):
-        if len(info) == 2:
-            self.numerator = numerator
-            self.denominator = denominator
-        elif len(info) == 0:
-            pass
-        else:
-            raise ValueError
+    def __init__(self, numerator=1, denominator=1):
+        """
+        @numerator The numerator of the fraction (default is 1)
+        @denominator The denominator of the fraction (default is 1)
+        """
+        self.numerator = numerator
+        self.denominator = denominator
 
     def simplify(self):
+        """
+        Simplifies the fraction to it's simplest form (2/4 -> 1/2)
+        """
         num = 2
         while True:
             if self.numerator % num == 0 and self.denominator % num == 0:
@@ -78,6 +78,9 @@ class Fraction:
             return f"{self.numerator}/{self.denominator}"
 
     def toDouble(self):
+        """
+        Converts a fraction to a double (1/2 -> 0.5)
+        """
         return self.numerator/self.denominator
     
     def __mod__(self, f):
